42d80bbf0cdb49aa83cdf2628012c60d5af9510e
TuanKietTran <35926768+TuanKietTran@users.noreply.github.com>
2020-11-28 22:56:54 +0700

    update main.js

--- main.js
+++ main.js
@@ -1,3 +1,108 @@
+<<<<<<< HEAD
+var problems = [
+    new Problem("Hyper Text Markup Language Stand For?", ["JavaScript", "XHTML", "CSS", "HTML"], "HTML"),
+    new Problem("Which language is used for styling web pages?", ["HTML", "JQuery", "CSS", "XML"], "CSS"),
+    new Problem("Which is not a JavaScript Framework?", ["Python Script", "JQuery", "Django", "NodeJS"], "Django"),
+    new Problem("Which is used for Connect To Database?", ["PHP", "HTML", "JS", "All"], "PHP"),
+    new Problem("Webdevtrick.com is about..", ["Web Design", "Graphic Design", "SEO & Development", "All"], "All"),
+    new Problem("What is 1+1=?", ["1", "2", "3", "4"], "2"),
+    new Problem("Who is the most handsome boy?", ["Tien", "Kiet", "Thai", "Nhan"], "Tien"),
+    new Problem("What do you do?", ["Dentist", "Doctor", "Dev", "Hacker"], "Hacker"),
+    new Problem("What is 2^6", ["I don't know", "231", "423", "64"], "64"),
+    new Problem("Who is the champion in RapViet show 2020", ["G-Ducky", "Ricky Star", "De Choat", "Yuno Bigboy"], "De Choat")
+];
+
+var quiz = new Quiz(problems);
+var timer = 10;
+var timeRemain = timer;
+quiz.show();
+
+
+
+
+
+
+
+
+
+// --------Timer-----------
+var idVar = setInterval(() => {
+    if (timer >= 0){
+        var minute = Math.floor(timer / 60);
+        if (minute / 10 < 1) minute = "0" + minute;
+        var second = timer % 60;
+        if (second / 10 < 1) second = "0" + second;
+        document.getElementById("timer").innerHTML = minute + ":" + second;
+        timer -= 1;
+    }
+    else {
+        showStats();
+        myStopFunction();
+        
+    }
+}, 1000);
+
+
+function myStopFunction() {
+    clearInterval(idVar);
+
+    document.getElementById("timer").innerHTML = '';
+    document.getElementById("timeLeft").style.visibility = "hidden";
+    timeRemain -= timer;
+    var minuteRemain = Math.floor(timeRemain / 60);
+    if (minuteRemain / 10 < 1) minuteRemain = "0" + minuteRemain;
+    var secondRemain = timeRemain % 60;
+    if (secondRemain / 10 < 1) secondRemain = "0" + secondRemain;
+    document.getElementById('timeResult').innerHTML = minuteRemain + ":" + secondRemain;
+    
+}
+
+function moveTo(value) {
+    if (value === "next") 
+    {
+        quiz.index++;
+    } 
+    else if (value == "back"){
+        quiz.index--;   
+    } 
+    else {
+        quiz.index = parseInt(value);
+    }
+    quiz.reset();
+    quiz.update();
+    quiz.show();
+}
+
+function showStats(){
+    
+    document.getElementById("container").innerHTML =
+        `<div id="mainContain">
+            <p id="font"><b>Your Result</b></p>
+            <div id="link">
+                <a class="rank" style="text-decoration: none;" href=""><b>Your Ranking</b></a>
+            </div>
+            <div id="score">
+                <h1 id="text"><b><u>YOUR SCORE</u></b></h1>
+                <div id="scoreBoard"></div>
+            </div>
+
+            <div class="timeResult">
+                <b style="float: left; margin-left: 80px;">Time</b>
+                <div id="timeResult"></div>
+            </div>
+        </div>
+
+    </div>`;
+    document.getElementById('scoreBoard').innerHTML = quiz.score;
+   
+    myStopFunction();
+}
+
+function showConsole(){
+    console.log("Hello Boiz");
+}
+
+=======
 var problems = [
     new Problem("Hyper Text Markup Language Stand For?", ["JavaScript", "XHTML", "CSS", "HTML"], "HTML"),
     new Problem("Which language is used for styling web pages?", ["HTML", "JQuery", "CSS", "XML"], "CSS"),
@@ -103,3 +208,4 @@ function showConsole(){
     console.log("Hello Boiz");
 }
 
+>>>>>>> 11a381891fef43dbe710289f9bdf4f9d874bd106
